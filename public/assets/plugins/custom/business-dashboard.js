947eada87693c6d79a3d68a9760e31dd502b46b8
samerhassan95 <arch.samerhassan@gmail.com>
2026-03-28 17:39:31 +0300

    Fix dashboard SVG rendering and update SAR color

--- public/assets/plugins/custom/business-dashboard.js
+++ public/assets/plugins/custom/business-dashboard.js
@@ -24,7 +24,7 @@ function currencyFormat(amount, type = "icon", decimals = 2) {
 
     // SAR Symbol SVG
     const sarSymbolSVG =
-        '<svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg" style="display: inline-block; vertical-align: middle; margin-left: 3px;"><g clip-path="url(#clip0_price_5-1)"><path d="M6.68122 10.6309C6.48962 11.0558 6.36297 11.5168 6.31445 12.0003L10.369 11.1384C10.5606 10.7137 10.6872 10.2525 10.7358 9.76904L6.68122 10.6309Z" fill="currentColor"/><path d="M10.3691 8.55619C10.5607 8.13144 10.6873 7.67031 10.7359 7.18683L7.57749 7.85857V6.56725L10.369 5.97403C10.5606 5.54929 10.6873 5.08815 10.7358 4.60467L7.57739 5.27584V0.631863C7.09343 0.903594 6.66363 1.2653 6.31425 1.69195V5.54441L5.05111 5.8129V0.000244141C4.56715 0.27188 4.13735 0.633678 3.78797 1.06033V6.08129L0.961685 6.68186C0.770089 7.1066 0.643345 7.56773 0.594729 8.05122L3.78797 7.3726V8.99879L0.365788 9.72601C0.174192 10.1508 0.0475433 10.6119 -0.000976562 11.0954L3.58109 10.3341C3.87269 10.2735 4.12331 10.1011 4.28625 9.86384L4.94318 8.8899V8.88971C5.01138 8.78895 5.05111 8.66746 5.05111 8.53661V7.10412L6.31425 6.83564V9.41827L10.369 8.55599L10.3691 8.55599Z" fill="currentColor"/></g><defs><clipPath id="clip0_price_5-1"><rect width="10.7368" height="12" fill="white"/></clipPath></defs></svg>';
+        '<svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg" style="display: inline-block; vertical-align: middle; margin-left: 3px;"><g clip-path="url(#clip0_price_5-1)"><path d="M6.68122 10.6309C6.48962 11.0558 6.36297 11.5168 6.31445 12.0003L10.369 11.1384C10.5606 10.7137 10.6872 10.2525 10.7358 9.76904L6.68122 10.6309Z" fill="#298000"/><path d="M10.3691 8.55619C10.5607 8.13144 10.6873 7.67031 10.7359 7.18683L7.57749 7.85857V6.56725L10.369 5.97403C10.5606 5.54929 10.6873 5.08815 10.7358 4.60467L7.57739 5.27584V0.631863C7.09343 0.903594 6.66363 1.2653 6.31425 1.69195V5.54441L5.05111 5.8129V0.000244141C4.56715 0.27188 4.13735 0.633678 3.78797 1.06033V6.08129L0.961685 6.68186C0.770089 7.1066 0.643345 7.56773 0.594729 8.05122L3.78797 7.3726V8.99879L0.365788 9.72601C0.174192 10.1508 0.0475433 10.6119 -0.000976562 11.0954L3.58109 10.3341C3.87269 10.2735 4.12331 10.1011 4.28625 9.86384L4.94318 8.8899V8.88971C5.01138 8.78895 5.05111 8.66746 5.05111 8.53661V7.10412L6.31425 6.83564V9.41827L10.369 8.55599L10.3691 8.55599Z" fill="#298000"/></g><defs><clipPath id="clip0_price_5-1"><rect width="10.7368" height="12" fill="white"/></clipPath></defs></svg>';
 
     // Check if currency is SAR
     const isSAR = code === "SAR" || symbol === "^";
@@ -203,24 +203,24 @@ function getDashboardData() {
         url: url,
         dataType: "json",
         success: function (res) {
-            $("#total_sales").text(res.total_sales);
-            $("#this_month_total_sales").text(res.this_month_total_sales);
-            $("#total_purchase").text(res.total_purchase);
-            $("#this_month_total_purchase").text(res.this_month_total_purchase);
-            $("#total_income").text(res.total_income);
-            $("#this_month_total_income").text(res.this_month_total_income);
-            $("#total_expense").text(res.total_expense);
-            $("#this_month_total_expense").text(res.this_month_total_expense);
-            $("#total_customer").text(res.total_customer);
-            $("#this_month_total_customer").text(res.this_month_total_customer);
-            $("#total_supplier").text(res.total_supplier);
-            $("#this_month_total_supplier").text(res.this_month_total_supplier);
-            $("#total_sales_return").text(res.total_sales_return);
-            $("#this_month_total_sale_return").text(
+            $("#total_sales").html(res.total_sales);
+            $("#this_month_total_sales").html(res.this_month_total_sales);
+            $("#total_purchase").html(res.total_purchase);
+            $("#this_month_total_purchase").html(res.this_month_total_purchase);
+            $("#total_income").html(res.total_income);
+            $("#this_month_total_income").html(res.this_month_total_income);
+            $("#total_expense").html(res.total_expense);
+            $("#this_month_total_expense").html(res.this_month_total_expense);
+            $("#total_customer").html(res.total_customer);
+            $("#this_month_total_customer").html(res.this_month_total_customer);
+            $("#total_supplier").html(res.total_supplier);
+            $("#this_month_total_supplier").html(res.this_month_total_supplier);
+            $("#total_sales_return").html(res.total_sales_return);
+            $("#this_month_total_sale_return").html(
                 res.this_month_total_sale_return,
             );
-            $("#total_purchase_return").text(res.total_purchase_return);
-            $("#this_month_total_purchase_return").text(
+            $("#total_purchase_return").html(res.total_purchase_return);
+            $("#this_month_total_purchase_return").html(
                 res.this_month_total_purchase_return,
             );
         },
